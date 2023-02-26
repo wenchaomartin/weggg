83aea7c6248c859001f0a2aa1da032233f9cf8b5
martin <wenchaomartin@gmail.com>
2023-02-26 11:08:35 +0800

    draw a red circle

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
-(()=>{
-    console.log("hello world!");
+(() => {
+    canvas = document.getElementById("canvas")
+    canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+    context = canvas.getContext('2d')
+    context.arc(100, 100, 50, 0, 2 * Math.PI, false);
+
+    context.fillStyle = '#FF0000';
+    context.fill();
 })()
