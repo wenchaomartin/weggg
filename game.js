0c2d4ebc2be83c9dc79fb6708e700386817d26d3
martin <wenchaomartin@gmail.com>
2023-02-26 12:09:14 +0800

    make circle move

--- game.js
+++ game.js
@@ -1,10 +1,32 @@
 (() => {
+    let r = 50
+    let cx = 100
+    let cy = 100
+    let speed = 20
     canvas = document.getElementById("canvas")
     canvas.height = window.innerHeight
     canvas.width = window.innerWidth
     context = canvas.getContext('2d')
-    context.arc(100, 100, 50, 0, 2 * Math.PI, false);
+    window.addEventListener('keydown',(e) => {
+        console.log(e.code)
+        switch(e.code){            
+            case "KeyA": cx -= speed ;break;
+            case "KeyD": cx += speed ;break;
+            case "KeyW": cy -= speed ;break;
+            case "KeyS": cy += speed ;break;
+        }
+    });
 
-    context.fillStyle = '#FF0000';
-    context.fill();
+    function step(timestamp) {
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        context.beginPath();
+        context.arc(cx, cy, r, 0, 2 * Math.PI, false);
+        context.fillStyle = '#FF0000';
+        context.fill();
+       
+        window.requestAnimationFrame(step)
+    }
+
+    window.requestAnimationFrame(step)
 })()
+
