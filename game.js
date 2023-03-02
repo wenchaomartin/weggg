9b034fdd27ff863621569eb1009105d287829856
martin <wenchaomartin@gmail.com>
2023-03-02 23:15:16 +0800

    bullet

--- game.js
+++ game.js
@@ -1,93 +1,113 @@
-class Vector {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    plus(that){
-        return new Vector(this.x + that.x, this.y + that.y);
-    }
-    scale(dt){
-        return new Vector(this.x *dt, this.y *dt);
-    }
+
+canvas = document.getElementById("canvas");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+context = canvas.getContext("2d");
+
+function draw(context, pos, r, color = "red") {
+  context.beginPath();
+  context.arc(pos.x, pos.y, r, 0, 2 * Math.PI, false);
+  context.fillStyle = color;
+  context.fill();
 }
 
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(that) {
+    return new Vector(this.x + that.x, this.y + that.y);
+  }
+  scale(dt) {
+    return new Vector(this.x * dt, this.y * dt);
+  }
+  minus(that) {
+    return new Vector(this.x - that.x, this.y - that.y);
+  }
+  len() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+  normal() {
+    return new Vector(this.x / this.len(), this.y / this.len());
+  }
+}
+
+class Bullet {
+  constructor(pos, vel) {
+    this.pos = pos;
+    this.vel = vel;
+  }
+
+  update(dt) {
+    this.pos = this.pos.plus(this.vel.scale(dt * bulletSpeed));
+    console.log(this.pos);
+  }
+  render(context) {
+    draw(context, this.pos, bullet_radius, "yellow");
+  }
+}
+
+let bullets = [];
 let keydown = new Set();
-let start=0;
-let circle = new Vector(100,100);
+let start = 0;
+let circle = new Vector(100, 100);
 
-
-
+let r = 50;
+let cx = 100;
+let cy = 100;
+let speed = 500;
+let bulletSpeed = 2000;
+let bullet_radius = 10;
+let directtionMap = {
+  KeyA: new Vector(-speed, 0),
+  KeyD: new Vector(speed, 0),
+  KeyW: new Vector(0, -speed),
+  KeyS: new Vector(0, speed),
+};
 
 (() => {
-    let r = 50
-    let cx = 100
-    let cy = 100
-    let speed = 500
-    //todo circle move not smooth
-    canvas = document.getElementById("canvas")
-    canvas.height = window.innerHeight
-    canvas.width = window.innerWidth
-    context = canvas.getContext('2d')
-
-    let directtionMap = {
-        "KeyA": new Vector(-speed, 0),
-        "KeyD": new Vector(speed, 0),
-        "KeyW": new Vector(0, -speed),
-        "KeyS": new Vector(0, speed)
-    }
-    window.addEventListener('keydown', (e) => {
-        switch (e.code) {
-            case "KeyA": keydown.add("KeyA"); break;
-            case "KeyD": keydown.add("KeyD"); break;
-            case "KeyW": keydown.add("KeyW");break;
-            case "KeyS": keydown.add("KeyS");break;
-        }
-    });
-
-    window.addEventListener('keyup', (e) => {
-        switch (e.code) {
-            case "KeyA": keydown.delete("KeyA");break;
-            case "KeyD": keydown.delete("KeyD");break;
-            case "KeyW": keydown.delete("KeyW");break;
-            case "KeyS": keydown.delete("KeyS");break;
-        }
-    });
-
-
-    function draw(context,pos,r){
-        console.log(circle.x, circle.y)
-        context.clearRect(0, 0, canvas.width, canvas.height)
-        context.beginPath();
-        context.arc(pos.x, pos.y , r, 0, 2 * Math.PI, false);
-        context.fillStyle = '#FF0000';
-        context.fill();
+  function step(timestamp) {
+    let dt = 0;
+    if (start == 0) {
+      start = timestamp;
+    } else {
+      dt = (timestamp - start) * 0.001;
+      start = timestamp;
     }
 
-
-    function step(timestamp) {
-        let dt = 0;
-        let sx = 0;
-        let sy = 0;
-        
-        if (start==0) {
-            start = timestamp
-        } else {
-            dt = (timestamp - start) * 0.001
-            start = timestamp
-        }
-
-        for(e of  keydown.values()){
-            if (e in directtionMap) {
-                let v = directtionMap[e]
-                circle = circle.plus(v.scale(dt))
-            }
-        }
-        
-        draw(context,circle,r)
-
-        window.requestAnimationFrame(step)
+    for (e of keydown.values()) {
+      if (e in directtionMap) {
+        let v = directtionMap[e];
+        circle = circle.plus(v.scale(dt));
+      }
     }
 
-    window.requestAnimationFrame(step)
-})()
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    bullets = bullets.filter(bullet => !(bullet.pos.x < 0 || bullet.pos.y < 0 || bullet.pos.x > canvas.width || bullet.pos.y > canvas.height))
+    for (b of bullets) {
+      b.update(dt);
+      b.render(context);
+    }
 
+    draw(context, circle, r);
+
+    window.requestAnimationFrame(step);
+  }
+
+  window.requestAnimationFrame(step);
+
+  window.addEventListener("keydown", (e) => {
+    keydown.add(e.code);
+  });
+
+  window.addEventListener("keyup", (e) => {
+    keydown.delete(e.code);
+  });
+
+  window.addEventListener("mousedown", (e) => {
+    let pos = new Vector(e.offsetX, e.offsetY);
+    let v = pos.minus(circle).normal();
+    bullets.push(new Bullet(circle, v));
+  });
+})();
