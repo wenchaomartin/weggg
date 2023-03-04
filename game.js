e30d7e505ee009cf7884d7edc2f9a1164d7c401b
martin <wenchaomartin@gmail.com>
2023-03-04 10:33:02 +0800

    refactor code extract game class and update render method

--- game.js
+++ game.js
@@ -33,6 +33,8 @@ class Vector {
   }
 }
 
+
+
 class Bullet {
   constructor(pos, vel) {
     this.pos = pos;
@@ -51,7 +53,6 @@ class Bullet {
 let bullets = [];
 let keydown = new Set();
 let start = 0;
-let circle = new Vector(100, 100);
 
 let r = 50;
 let cx = 100;
@@ -59,14 +60,75 @@ let cy = 100;
 let speed = 500;
 let bulletSpeed = 2000;
 let bullet_radius = 10;
-let directtionMap = {
+let directionMap = {
   KeyA: new Vector(-speed, 0),
   KeyD: new Vector(speed, 0),
   KeyW: new Vector(0, -speed),
   KeyS: new Vector(0, speed),
 };
 
+class Player{
+  constructor(pos,vel){
+    this.pos = pos
+    this.vel =vel
+  }
+
+  render(context){
+      draw(context,this.pos,r);
+  }
+}
+
+class Game{
+  constructor(){
+    this.player = new Player(new Vector(100, 100),new Vector(0,0))
+    this.bullets = []
+    this.keyPressed = new Set();
+  }
+
+  update(dt){
+
+      for ( let e of this.keyPressed){
+        if (e in directionMap){
+            this.player.pos = this.player.pos.plus(directionMap[e].scale(dt))
+        }
+      }
+
+      this.bullets = bullets.filter(bullet => !(bullet.pos.x < 0 || bullet.pos.y < 0 || bullet.pos.x > canvas.width || bullet.pos.y > canvas.height))
+      for(let b of bullets){
+          b.update(dt)
+      }
+      
+  }
+
+  render(context){
+    this.player.render(context)
+    for(let b of this.bullets){
+      b.render(context)
+    }
+  }
+
+  keydown(e){
+    this.keyPressed.add(e.code);
+
+  }
+
+  keyup(e){
+    this.keyPressed.delete(e.code);
+  }
+
+  mousedown(e){
+    let pos = new Vector(e.offsetX, e.offsetY);
+    let v = pos.minus(this.player.pos).normal();
+    bullets.push(new Bullet(this.player.pos, v));
+  }
+  
+}
+
+
+
 (() => {
+  let game = new Game();
+  
   function step(timestamp) {
     let dt = 0;
     if (start == 0) {
@@ -75,22 +137,9 @@ let directtionMap = {
       dt = (timestamp - start) * 0.001;
       start = timestamp;
     }
-
-    for (e of keydown.values()) {
-      if (e in directtionMap) {
-        let v = directtionMap[e];
-        circle = circle.plus(v.scale(dt));
-      }
-    }
-
     context.clearRect(0, 0, canvas.width, canvas.height);
-    bullets = bullets.filter(bullet => !(bullet.pos.x < 0 || bullet.pos.y < 0 || bullet.pos.x > canvas.width || bullet.pos.y > canvas.height))
-    for (b of bullets) {
-      b.update(dt);
-      b.render(context);
-    }
-
-    draw(context, circle, r);
+    game.update(dt)
+    game.render(context);
 
     window.requestAnimationFrame(step);
   }
@@ -98,16 +147,15 @@ let directtionMap = {
   window.requestAnimationFrame(step);
 
   window.addEventListener("keydown", (e) => {
-    keydown.add(e.code);
+    game.keydown(e);
   });
 
   window.addEventListener("keyup", (e) => {
-    keydown.delete(e.code);
+    game.keyup(e)
   });
 
   window.addEventListener("mousedown", (e) => {
-    let pos = new Vector(e.offsetX, e.offsetY);
-    let v = pos.minus(circle).normal();
-    bullets.push(new Bullet(circle, v));
+    game.mousedown(e);
   });
 })();
+
