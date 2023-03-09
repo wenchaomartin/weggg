27f90ff804e901d7bc0e2bc2c8163f8d79cf7423
songwenchao <songwenchao@111.com.cn>
2023-03-09 12:57:30 +0800

    burst partcles

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ let width = canvas.width;
 let distance = 2000
 
 context = canvas.getContext("2d");
+let N = Math.random() * 6 + 1
 
 function draw(context, pos, r, color = "red") {
     context.beginPath();
@@ -16,6 +17,7 @@ function draw(context, pos, r, color = "red") {
     context.fillStyle = color;
     context.fill();
 }
+
 
 class Vector {
     constructor(x, y) {
@@ -47,6 +49,13 @@ class Vector {
         return new Vector(len * Math.cos(angle), len * Math.sin(angle))
     }
 
+    distance(that) {
+        let dx = this.x - that.x
+        let dy = this.y - that.y
+
+        return Math.sqrt(dx * dx + dy * dy)
+    }
+
 }
 
 
@@ -75,10 +84,12 @@ let cx = 100;
 let cy = 100;
 let speed = 500;
 let bulletSpeed = 2000;
+let particleSpeed = 2000;
 let bullet_radius = 10;
 let enemySpeed = 200
 let spawnRate = 1
 let currentRate = spawnRate
+let particle_radius =10
 
 let directionMap = {
     KeyA: new Vector(-speed, 0),
@@ -97,6 +108,23 @@ class Player {
         draw(context, this.pos, r);
     }
 }
+
+class Particle {
+    constructor(pos,vel) {
+        this.pos = pos
+        this.vel = vel
+        this.r = particle_radius
+    }
+
+    update(dt) {
+        this.pos = this.pos.plus(this.vel.scale(dt*particleSpeed))
+    }
+
+    render(context) {
+        draw(context, this.pos, this.r, 'blue');
+    }
+}
+
 
 class Enemy {
     constructor(pos) {
@@ -124,6 +152,7 @@ class Game {
         this.bullets = []
         this.keyPressed = new Set();
         this.enermies = new Set();
+        this.particles = []
 
     }
 
@@ -139,14 +168,28 @@ class Game {
         for (let b of bullets) {
             b.update(dt)
         }
+
         currentRate = currentRate - dt
         if (currentRate < 0) {
             currentRate = spawnRate
             this.enermies.add(spawnEnemies(this.player.pos))
         }
 
+        for (let b of bullets) {
+            for (let e of this.enermies) {
+                if (b.pos.distance(e.pos)<r + bullet_radius) {
+                    let N = Math.random()*6+1
+                    for(let i=0;i<N;i++) {
+                        let angle = Math.random()*2*Math.PI
+                        this.particles.push(new Particle(e.pos,Vector.proV2(1,angle)))
+                    }
+                }
+            }
+        }
+
 
         this.enermies.forEach(e => e.update(dt, this.player.pos))
+        this.particles.forEach(e => e.update(dt))
 
     }
 
@@ -159,6 +202,8 @@ class Game {
 
 
         this.enermies.forEach(e => e.render(context))
+        this.particles.forEach(e => e.render(context))
+
     }
 
     keydown(e) {
